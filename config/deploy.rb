db4714a6ba70cbeaea9c0280805c57c186024ae0
EugeneKey <evgeny.konyaev@gmail.com>
2017-04-10 21:56:10 +0700

    capistrano setup
    puma and nginx config setup

---
+++ config/deploy.rb
@@ -0,0 +1,98 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :rvm_ruby_version, '2.3.1@sharetribe'
+set :nvm_type, :user # or :system, depends on your nvm setup
+set :nvm_node, 'v6.9.5'
+set :nvm_map_bins, %w{node npm yarn}
+
+set :repo_url,        'git@github.com:EugeneKey/sharetribe.git'
+set :application,     'sharetribe'
+set :user,            'deployer'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] || 'master'
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, [])
+        .push('config/database.yml', 'config/config.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, [])
+        .push('bin', 'log', 'tmp/pids', 'tmp/cache',
+              'tmp/sockets', 'vendor/bundle', 'public/system',
+              'public/uploads', 'client/node_modules', 'public/assets')
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/deploy`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+
+end
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
